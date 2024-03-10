fcb880cd894e8a47e0f19fa618ef347af2cf613f
hayato-oo <xuhiko13@gmail.com>
2024-03-10 04:16:20 -0400

    frustration fixed lmaolmaolmao

--- hosts/default/system/network/default.nix
+++ hosts/default/system/network/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   networking.hostName = "nixos";
@@ -21,6 +21,53 @@
     '';
 
   };
+  
+  
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  security.polkit.enable = true;
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+          )
+        )
+      {
+        return polkit.Result.YES;
+      }
+    })
+  '';
+  
+  systemd.services.networkManagerRestart = {
+    description = "Restart NetworkManger.service every reboot";
+    #script = [ pkgs.coreutils bin/networkmanager restart ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.networkmanager}/bin/NetworkManager restart";
+      Restart = "always";
+    };
+  };
+
 
   # Don't wait for network startup
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
