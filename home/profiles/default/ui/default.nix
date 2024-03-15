82046ad688a0bd2a23f2191397251dfa6df68c88
hayato-oo <xuhiko13@gmail.com>
2024-03-15 01:48:10 -0400

    added custom cursor, added default editor

--- home/profiles/default/ui/default.nix
+++ home/profiles/default/ui/default.nix
@@ -8,6 +8,7 @@
   ./kitty
   ./neofetch
   ./eww
+  ./cursor
 
   ];
 
--- home/profiles/default/ui/kitty/default.nix
+++ home/profiles/default/ui/kitty/default.nix
@@ -8,6 +8,13 @@
 
   programs.kitty = {
     enable = true;
+    #theme = "N0tch2k";
+    #extraConfig = ''
+    #  window_margin_width 9
+    #  background_opacity 0.78
+    #  #foreground #baafa9
+    #  background #010100
+    #'';
 
   };
   
