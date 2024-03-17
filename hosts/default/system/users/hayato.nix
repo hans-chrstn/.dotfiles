9051c453bc9e5cc8fba7915ab2b655d5d6ccc66a
hayato-oo <xuhiko13@gmail.com>
2024-03-16 23:13:39 -0400

    added ssh services to hayato

--- hosts/default/system/users/hayato.nix
+++ hosts/default/system/users/hayato.nix
@@ -22,4 +22,13 @@
       ];
     };
   };
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      PasswordAuthentication = false;
+    };
+  };
 }
