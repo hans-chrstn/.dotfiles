df7478cb4c6db9284fcf9dcc2f3fc706d21c992c
hayato-oo <xuhiko13@gmail.com>
2024-03-21 14:41:21 -0400

    could be a fix to duplicates on startup

--- hosts/default/system/network/default.nix
+++ hosts/default/system/network/default.nix
@@ -23,40 +23,40 @@
   };
   
   
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
+#  systemd = {
+#    user.services.polkit-gnome-authentication-agent-1 = {
+#      description = "polkit-gnome-authentication-agent-1";
+#      wantedBy = [ "graphical-session.target" ];
+#      wants = [ "graphical-session.target" ];
+#      after = [ "graphical-session.target" ];
+#      serviceConfig = {
+#        Type = "simple";
+#        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+#        Restart = "on-failure";
+#        RestartSec = 1;
+#        TimeoutStopSec = 10;
+#      };
+#    };
+#  };
 
   security.polkit.enable = true;
 
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if (
-        subject.isInGroup("users")
-          && (
-            action.id == "org.freedesktop.login1.reboot" ||
-            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-            action.id == "org.freedesktop.login1.power-off" ||
-            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-          )
-        )
-      {
-        return polkit.Result.YES;
-      }
-    })
-  '';
+#  security.polkit.extraConfig = ''
+#    polkit.addRule(function(action, subject) {
+#      if (
+#        subject.isInGroup("users")
+#          && (
+#            action.id == "org.freedesktop.login1.reboot" ||
+#            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+#            action.id == "org.freedesktop.login1.power-off" ||
+#            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+#          )
+#        )
+#      {
+#        return polkit.Result.YES;
+#      }
+#    })
+#  '';
   
   systemd.services.networkManagerRestart = {
     description = "Restart NetworkManger.service every reboot";
--- hosts/default/system/services/default.nix
+++ hosts/default/system/services/default.nix
@@ -15,10 +15,10 @@
   services = {
     tumbler.enable = true;
     gvfs.enable = true;
-    gnome = {
-      sushi.enable = true;
-      gnome-keyring.enable = true;
-    };
+#    gnome = {
+#      sushi.enable = true;
+#      gnome-keyring.enable = true;
+#    };
     dbus = {
       enable = true;
       implementation = "broker";
@@ -29,7 +29,7 @@
   #portals
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+#    wlr.enable = true;
     extraPortals = [ 
       pkgs.xdg-desktop-portal-gtk 
       pkgs.xdg-desktop-portal
