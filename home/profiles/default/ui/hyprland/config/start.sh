db25504abf1b646ca8cf6515a8a0c30642dce83a
hayato-oo <xuhiko13@gmail.com>
2024-03-27 02:14:54 -0400

    fixed ags-hyprland exec

--- home/profiles/default/ui/hyprland/config/start.sh
+++ home/profiles/default/ui/hyprland/config/start.sh
@@ -3,9 +3,6 @@ hyprlock &
 hypridle&
 hyprpaper&
 #ags -c ~/.dotfiles/home/profiles/default/ui/ags/config/config.js &
-sleep 5 &
-eww daemon &
-sleep 15 &
-eww open bar &
+ags -c ~/.dotfiles/home/profiles/default/ui/ags/ags/config.js &
 wl-gammarelay-rs
 
