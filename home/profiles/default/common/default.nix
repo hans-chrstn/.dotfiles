b38d5c2adfc33e6259d22aad99fe0f96038f06a6
hayato-oo <xuhiko13@gmail.com>
2024-04-13 20:25:13 -0400

    removed unused icons from common

--- home/profiles/default/common/default.nix
+++ home/profiles/default/common/default.nix
@@ -1,13 +1,11 @@
 { lib, config, pkgs, ... }:
 let
   apple-fonts = pkgs.callPackage ./ttf/apple-fonts.nix { };
-  weathericonserik = pkgs.callPackage ./icons/erikweather.nix { };
 in
 
 {
   fonts.fontconfig.enable = true;
   home.packages = [
-    weathericonserik
     apple-fonts
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
   ];
