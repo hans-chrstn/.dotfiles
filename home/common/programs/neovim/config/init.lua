1cc13ab05e0148da678985814b66bac29d9838de
hayato-oo <xuhiko13@gmail.com>
2024-06-08 17:35:01 -0400

    added gitsigns, fixed neo-tree keymaps, removed wrong keymap from telescope

--- home/common/programs/neovim/config/init.lua
+++ home/common/programs/neovim/config/init.lua
@@ -10,6 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 require("vim-options")
 require("lazy").setup("plugins")
 
--- home/common/programs/neovim/config/lua/plugins/toggleterm.lua
+++ home/common/programs/neovim/config/lua/plugins/toggleterm.lua
@@ -4,6 +4,12 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>o", ":ToggleTerm name=$fx <CR>", {})
     vim.keymap.set("n", "<leader>0", ":ToggleTerm direction=vertical name=$fx <CR>", {})
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+    vim.keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
     require("toggleterm").setup({
       size = function(term)
         if term.direction == "horizontal" then
