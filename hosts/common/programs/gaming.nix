b699bf4ee8fd9c33891106c69603a88b72e99ac0
hayato-oo <xuhiko13@gmail.com>
2024-08-15 20:50:04 -0400

    #7

--- hosts/common/programs/gaming.nix
+++ hosts/common/programs/gaming.nix
@@ -1,22 +1,5 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # GAMING
-    sunshine
-    winetricks
-    steam-run
-    (lutris.override {
-      extraPkgs = pkgs: [
-        
-      ];
-    })
-    (wineWowPackages.full.override {
-      wineRelease = "stable";
-      mingwSupport = true;
-    })
-    protontricks
-    cartridges
-    heroic
-    gogdl
   ];
 }
