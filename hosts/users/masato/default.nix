6e2501905f2938406b0737e719c39c212221954b
hayato-oo <xuhiko13@gmail.com>
2025-03-10 22:25:25 -0400

    #35
    added virtualise module to masato and mishima

--- hosts/users/masato/default.nix
+++ hosts/users/masato/default.nix
@@ -2,9 +2,14 @@
 {
   imports = [
     ../../common/masato.nix
+    ./overlays.nix
   ] ++ (builtins.attrValues outputs.nixosModules);
 
   environment.systemPackages = [
     inputs.hyprsysteminfo.packages.${pkgs.system}.default
   ];
+  programs.virtualise = {
+    enable = true;
+    proxmoxIP = "10.0.0.21";
+  };
 }
