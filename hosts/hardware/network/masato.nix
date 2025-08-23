e6a039bb405b00fc73304fdae7b94ad2a4fe9f4a
hayato-oo <xuhiko13@gmail.com>
2025-08-23 19:52:07 -0400

    #60
    added starbound server ports
    added nix-ld to masato

--- hosts/hardware/network/masato.nix
+++ hosts/hardware/network/masato.nix
@@ -4,8 +4,11 @@
     hostName = "nixos-server";  # DISCOVERABLE NETWORK NAME
     firewall = {
       allowedTCPPorts = [
+        21025 # starbound
+        21026 # starbound
       ];
       allowedUDPPorts = [
+        21025 # starbound
       ];
       allowedUDPPortRanges = [
       ];
