71de30ea72c5dfeb17b3fb63fda4ebf15d421ed7
hayato-oo <xuhiko13@gmail.com>
2025-10-21 10:59:23 -0400

    (fix): fixed intel, laptop, sops modules
    * fixed intel modules options, config
    * fixed laptop module not having a power profile
    * fixed sops for yu

--- hosts/yu/sops.nix
+++ hosts/yu/sops.nix
@@ -7,14 +7,16 @@
     inputs.sops-nix.nixosModules.sops
   ];
   sops = {
-    defaultSopsFile = ../../../../secrets/secrets.yaml;
+    defaultSopsFile = ../../secrets/secrets.yaml;
     age = {
       sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
     secrets = {
-      "users/jin/password" = {};
+      "users/jin/password" = {
+        neededForUsers = true;
+      };
       "networks/yu/interface/private-key" = {};
       "networks/yu/interface/address" = {};
       "networks/yu/interface/dns" = {};
--- modules/nixos/laptop/default.nix
+++ modules/nixos/laptop/default.nix
@@ -7,6 +7,11 @@
 in {
   options.mod.hardware.laptop = {
     enable = lib.mkEnableOption "Enable the laptop feature";
+    powerOptions = lib.mkOption {
+      type = lib.types.enum ["performance" "powersave"];
+      default = "powersave";
+      description = "Select either performance | powersave";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -15,6 +20,15 @@ in {
         "acpi_call"
       ];
       extraModulePackages = with config.boot.kernelPackages; [acpi_call];
+    };
+
+    services.power-profiles-daemon.enable = true;
+    powerManagement = {
+      enable = true;
+      cpuFreqGovernor = if cfg.powerOptions == "powersave" then
+        "powersave"
+      else
+        "performance";
     };
   };
 }
