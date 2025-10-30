533d56ec24e23dda762bcca335a075f14ffb59c4
hayato-oo <xuhiko13@gmail.com>
2025-10-29 20:53:49 -0400

    (fix): removed iscsi module, fixed rei config

--- modules/nixos/netfs/default.nix
+++ modules/nixos/netfs/default.nix
@@ -7,7 +7,6 @@
 in {
   options.mod.netfs = {
     enableNfs = lib.mkEnableOption "Enable the nfs feature";
-    enableIscsi = lib.mkEnableOption "Enable the iscsi feature";
     smb.enable = lib.mkEnableOption "Enable the smb feature";
     smb.path = lib.mkOption {
       type = lib.types.string;
@@ -18,8 +17,8 @@ in {
 
   config = lib.mkMerge [
     (lib.mkIf cfg.enableNfs {
+      services.nfs.server.enable = true;
       services.nfs = {
-        enable = true;
         settings = {
           nfsd = {
             threads = 8;
@@ -37,33 +36,6 @@ in {
             threads = 4;
           };
         };
-      };
-    })
-    (lib.mkIf cfg.enableIscsi {
-      services.openiscsi = {
-        enable = true;
-        extraConfig = ''
-          node.startup = automatic
-          node.session.initial_login_retry_max = 8
-          node.session.timeo.replacement_timeout = 120
-          node.conn[0].timeo.login_timeout = 15
-          node.conn[0].timeo.logout_timeout = 15
-          node.conn[0].timeo.noop_out_interval = 5
-          node.conn[0].timeo.noop_out_timeout = 5
-          node.session.cmds_max = 128
-          node.session.queue_depth = 32
-          node.session.iscsi.ImmediateData = Yes
-          node.session.iscsi.InitialR2T = No
-          node.session.iscsi.FirstBurstLength = 262144
-          node.session.iscsi.MaxBurstLength = 16776192
-          node.conn[0].iscsi.MaxRecvDataSegmentLength = 262144
-          node.conn[0].iscsi.MaxXmitDataSegmentLength = 0
-          node.conn[0].iscsi.HeaderDigest = CRC32C,None
-          node.conn[0].iscsi.DataDigest = CRC32C,None
-          node.session.nr_sessions = 1
-          node.session.iscsi.FastAbort = Yes
-          node.session.scan = auto
-        '';
       };
     })
     (lib.mkIf cfg.smb.enable {
